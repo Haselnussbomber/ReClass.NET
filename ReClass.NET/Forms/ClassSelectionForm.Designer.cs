f5f97fcf698ce3344444b494bb266f41da8585bc
Haselnussbomber <mail@haselnussbomber.de>
2025-04-28 13:40:03 +0200

    Add arrow key selection in Class Selection window

--- ReClass.NET/Forms/ClassSelectionForm.Designer.cs
+++ ReClass.NET/Forms/ClassSelectionForm.Designer.cs
@@ -73,7 +73,7 @@ namespace ReClassNET.Forms
 			this.filterNameTextBox.Size = new System.Drawing.Size(492, 20);
 			this.filterNameTextBox.TabIndex = 1;
 			this.filterNameTextBox.TextChanged += new System.EventHandler(this.filterNameTextBox_TextChanged);
-			// 
+			this.filterNameTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.filterNameTextBox_KeyDown);
 			// cancelButton
 			// 
 			this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
